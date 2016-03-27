10a80d21ec33e354d2eb9378922b57481660329d
Scott Sinclair <scottysinclair@gmail.com>
2016-03-27 22:57:08 +0200

    form is now rendering from barleyrs generated JSON schema.

--- src/index.jsx
+++ src/index.jsx
@@ -9,6 +9,26 @@ import transform from 'tcomb-json-schema';
 const Form = t.form.Form;
 
 
+let formSchema = transform({
+	"title": "None",
+	"type": "object",
+	"properties": {
+		"firstName": {
+			"type": "string"
+		}
+	}
+});
+
+
+fetch('/barleyrs/entitytypes/scott.picdb/Picture')
+.then(function(response) {
+  return response.json()
+}).then(function(json) {
+	formSchema = transform( json );
+	render(<App />, document.getElementById('app'));
+});
+
+
 // define your domain model with tcomb
 /*
 const Person = t.struct({
@@ -20,6 +40,7 @@ const Person = t.struct({
 /*
  * or with JSON schema
  */
+/*
 const Person = transform({
 	"title": "Person",
 	"type": "object",
@@ -30,9 +51,6 @@ const Person = transform({
 		"lastName": {
 			"type": "string"
 		},
-		"sex": {
-			"type": "string"
-		},
 		"age": {
 			"description": "Age in years",
 			"type": "integer",
@@ -41,7 +59,7 @@ const Person = transform({
 	},
 	"required": ["firstName", "lastName", "sex"]
 });
-
+*/
 
 const App = React.createClass({
 
@@ -64,7 +82,7 @@ const App = React.createClass({
     return (
     	<div>
     	<Form ref="form" 
-    		  type={Person}
+    		  type={formSchema}
     		  onChange={this.onChange} />
     		<button onClick={this.save}>Save</button>
     	</div>
@@ -75,3 +93,114 @@ const App = React.createClass({
 
 render(<App />, document.getElementById('app'));
 
+
+
+/*		"lastName2": {
+"type": "string"
+},
+"lastName3": {
+"type": "string"
+},
+"lastName4": {
+"type": "string"
+},
+"lastName5": {
+"type": "string"
+},
+"lastName6": {
+"type": "string"
+},
+"lastName7": {
+"type": "string"
+},
+"lastName8": {
+"type": "string"
+},
+"lastName9": {
+"type": "string"
+},
+"lastName11": {
+"type": "string"
+},
+"lastName12": {
+"type": "string"
+},
+"lastName13": {
+"type": "string"
+},
+"lastName14": {
+"type": "string"
+},
+"lastName15": {
+"type": "string"
+},
+"lastName16": {
+"type": "string"
+},
+"lastName17": {
+"type": "string"
+},
+"lastName18": {
+"type": "string"
+},
+"lastName19": {
+"type": "string"
+},
+"lastName111": {
+"type": "string"
+},
+"lastName112": {
+"type": "string"
+},
+"lastName113": {
+"type": "string"
+},
+"lastName114": {
+"type": "string"
+},
+"lastName115": {
+"type": "string"
+},
+"lastName116": {
+"type": "string"
+},
+"lastName117": {
+"type": "string"
+},
+"lastName118": {
+"type": "string"
+},
+"lastName119": {
+"type": "string"
+},
+"lastName1111": {
+"type": "string"
+},
+"lastName1112": {
+"type": "string"
+},
+"lastName1113": {
+"type": "string"
+},
+"lastName1114": {
+"type": "string"
+},
+"lastName1115": {
+"type": "string"
+},
+"lastName1116": {
+"type": "string"
+},
+"lastName1117": {
+"type": "string"
+},
+"lastName1118": {
+"type": "string"
+},
+"lastName1119": {
+"type": "string"
+},
+"sex": {
+"type": "string"
+},
+*/
